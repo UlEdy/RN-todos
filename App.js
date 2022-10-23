c534b9f895fc0d22ea50533bc94905f4a8c49a02
Edyta Ułanowicz <ulanowicz.edyta@gmail.com>
2022-10-23 14:20:04 +0200

    update

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { useState } from 'react';
+import Header from './components/Header';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [todos, setTodos] = useState([
+		{ text: 'buy coffee', key: '1' },
+		{ text: 'create an app', key: '2' },
+		{ text: 'play on the switch', key: '3' },
+	]);
+
+	return (
+		<View style={styles.container}>
+			{/* header */}
+			<Header />
+			<View style={styles.content}>
+				{/*to do forms */}
+				<View style={styles.list}>
+					<FlatList
+						data={todos}
+						renderItem={({ item }) => <Text>{item.text}</Text>}
+					/>
+				</View>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
+	content: {
+		padding: 40,
+	},
+	list: {
+		marginTop: 20,
+	},
 });
