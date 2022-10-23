7297ed8b2ee8aabc09c329be6ecb299ee8b1f240
Edyta Ułanowicz <ulanowicz.edyta@gmail.com>
2022-10-23 15:40:37 +0200

    add input field and button

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { useState } from 'react';
 import Header from './components/Header';
+import TodoItem from './components/TodoItem';
+import AddTodo from './components/AddTodo';
 
 export default function App() {
 	const [todos, setTodos] = useState([
@@ -9,16 +11,31 @@ export default function App() {
 		{ text: 'play on the switch', key: '3' },
 	]);
 
+	const pressHandler = (key) => {
+		setTodos((prevTodos) => {
+			return prevTodos.filter((todo) => todo.key != key);
+		});
+	};
+
+	const submitHandler = (text) => {
+		setTodos((prevTodos) => {
+			return [{ text: text, key: Math.random().toString }, ...prevTodos];
+		});
+	};
+
 	return (
 		<View style={styles.container}>
 			{/* header */}
 			<Header />
 			<View style={styles.content}>
 				{/*to do forms */}
+				<AddTodo submitHandler={submitHandler} />
 				<View style={styles.list}>
 					<FlatList
 						data={todos}
-						renderItem={({ item }) => <Text>{item.text}</Text>}
+						renderItem={({ item }) => (
+							<TodoItem item={item} pressHandler={pressHandler} />
+						)}
 					/>
 				</View>
 			</View>
